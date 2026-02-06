3a308fcd095145f1869f99def1a4b99dda39023b
rahimdz1 <kidstaha6@gmail.com>
2026-02-07 00:53:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // يجعل المسارات نسبية لتعمل على GitHub Pages دون مشاكل
   server: {
     port: 3000,
   },
